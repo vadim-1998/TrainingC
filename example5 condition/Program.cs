829b3773152cdead3fe783d992f7f658f7bd77fc
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-05-18 23:34:12 +1000

    example5

---
+++ example5 condition/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "vadim")
+{
+    Console.WriteLine("hi the best user");
+}
+else 
+{
+    Console.Write("hello, ");
+    Console.WriteLine(username);
+}
