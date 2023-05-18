c5061a65b04e9a0d3cebc64aa60ab32d830990e5
Vadim Zimnukhov <zimnukhovvadim24@gmail.com>
2023-05-18 23:43:51 +1000

    example6

---
+++ example6 base/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 5;
+int b = 3;
+int c = 4;
+int d = 8;
+int e = 2;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max= ");
+Console.WriteLine(max);
